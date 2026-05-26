a555f9a9f6597c0a4d4c8fe2bf393e4749c3779f
ricarsurbusiness <ricarsurbusiness@gmail.com>
2026-05-26 14:00:00 -0500

    feat: add new IP for CORS

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -104,7 +104,7 @@ func main() {
 
 	// Configuración CORS
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000", "http://18.117.195.172"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
